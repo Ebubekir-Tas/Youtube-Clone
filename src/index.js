ba82e7907a68f7bbe0ec8cddd50ffb6866306300
ebubekir-tas <tas_ebubekir@yahoo.com>
2021-05-23 13:13:12 -0400

    added browser router to index js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter } from 'react-router-dom';
 import store from './redux/RootStore'
 import { Provider } from 'react-redux'
 
 const rootElement = document.getElementById("root");
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>,
 
     rootElement
