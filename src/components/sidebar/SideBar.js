1ed44f58bf6475fa0f24b93905dcb1274ad4a63d
ebubekir-tas <tas_ebubekir@yahoo.com>
2021-05-25 20:36:43 -0400

    side bar routes

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./SideBar.css";
 import SideBarRow from "./SideBarRow";
 import { Home, Whatshot, Subscriptions, VideoLibrary, History, OndemandVideo, WatchLater, ThumbUp } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 const SideBar = () => {
   return (
     <aside className="sidebar">
-      <SideBarRow selected Icon={Home} title="Home" />
+      <Link to={{ pathname: '/' }}>
+        <SideBarRow selected Icon={Home} title="Home" />
+      </Link>
+
       <SideBarRow Icon={Whatshot} title="Trending" />
       <SideBarRow Icon={Subscriptions} title="Subscription" />
       <hr className="break-line" />
@@ -14,7 +18,10 @@ const SideBar = () => {
       <SideBarRow Icon={History} title="History" />
       <SideBarRow Icon={OndemandVideo} title="Your videos" />
       <SideBarRow Icon={WatchLater} title="Watch later" />
-      <SideBarRow Icon={ThumbUp} title="Liked videos" />
+      
+      <Link to={{ pathname: '/liked-videos' }}>
+        <SideBarRow Icon={ThumbUp} title="Liked videos" />
+      </Link>
       <hr className="break-line" />
     </aside>
   );
