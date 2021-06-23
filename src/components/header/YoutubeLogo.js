29ccd07cc9cf2e359ca725a912cc2d468da0bb13
Ebubekir-Tas <tas_ebubekir@yahoo.com>
2021-06-23 14:25:56 -0400

    youtube logo component fragment

--- src/components/header/YoutubeLogo.js
+++ src/components/header/YoutubeLogo.js
@@ -2,10 +2,9 @@ import React from "react";
 import "./YoutubeLogo.css";
 
 export function YoutubeLogo() {
-  let url = "#";
   return (
-    <a href={url}>
+    <>
       <img src={'/images/youtube-logo.png'} alt="YouTube Logo" className="youtube-logo" />
-    </a>
+    </>
   );
 }
