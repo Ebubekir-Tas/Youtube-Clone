73d306d22701db3da02b74e231e59dc06c8d923d
Ebubekir-Tas <tas_ebubekir@yahoo.com>
2021-06-23 15:18:28 -0400

    deleted menu icon links

--- src/components/header/MenuIcons.js
+++ src/components/header/MenuIcons.js
@@ -5,22 +5,14 @@ export function MenuIcons() {
   let url = "#";
   return (
     <div className="menu-icons">
-      <a href={url}>
-        <img src='/images/video-plus.svg' alt="Upload Video" />
-      </a>
-      <a href={url}>
-        <img src='/images/apps.svg' alt="Apps" />
-      </a>
-      <a href={url}>
-        <img src='/images/bell.svg' alt="Notifications" />
-      </a>
-      <a href={url}>
-        <img
-          className="menu-channel-icon"
-          src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*"
-          alt="Your Channel"
-        />
-      </a>
+      <img src='/images/video-plus.svg' alt="Upload Video" />
+      <img src='/images/apps.svg' alt="Apps" />
+      <img src='/images/bell.svg' alt="Notifications" />
+      <img
+        className="menu-channel-icon"
+        src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*"
+        alt="Your Channel"
+      />
     </div>
   );
 }
